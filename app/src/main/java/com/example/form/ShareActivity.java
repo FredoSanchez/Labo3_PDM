92e26f566bac4e0a0bfef2f9ce15865e58d8863b
Fredy Sanchez <00082817@uca.edu.sv>
2019-03-25 11:37:27 -0600

    Declaraciones

--- app/src/main/java/com/example/form/ShareActivity.java
+++ app/src/main/java/com/example/form/ShareActivity.java
@@ -2,8 +2,12 @@ package com.example.form;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ShareActivity extends AppCompatActivity {
+
+    TextView mSharedName, mSharedGender, mSharedEmail, mSharedPassword;
+    String data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
